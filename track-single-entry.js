afcfbe77564ca0ad387725586645bfb8c5c41cda
Todd Austin <toddaustin@users.noreply.github.com>
2016-05-23 12:09:36 -0700

    Update track-single-entry.js

--- track-single-entry.js
+++ track-single-entry.js
@@ -1,26 +1,25 @@
-<script>
 mw.setConfig('Kaltura.EnableEmbedUiConfJs', true);
        
-    var urlObject = {
-        "0_djht56n5" : {url: "http://www.example.com}
-    };
+var urlObject = {
+ "0_djht56n5" : {url: "http://www.example.com}
+};
 
-    function trackUsage(myURL) {
-    // #track-uage is an hidden iframe
-        $('#track-usage').attr("src", myURL);
-    };
+function trackUsage(myURL) {
+// #track-uage is an hidden iframe
+ $('#track-usage').attr("src", myURL);
+};
 
-    function trackURL(myIndex) {
-        var myURL= urlObject[myIndex].url;
-        trackUsage(myURL);
-    }
+function trackURL(myIndex) {
+ var myURL= urlObject[myIndex].url;
+ trackUsage(myURL);
+}
 
-    kWidget.addReadyCallback( function( playerId ){
-        kdp = document.getElementById( playerId );
-        kdp.kBind( "doPlay.test", function(){ //notice the .test postfix namespaces the event)
-            var entry_id = kdp.evaluate('{mediaProxy.entry.id}');
-            trackURL(entry_id);
-            kdp.kUnbind('.test');
-        });
-    });
-    </script>
+kWidget.addReadyCallback( function( playerId ){
+ kdp = document.getElementById( playerId );
+ kdp.kBind( "doPlay.test", function(){ //notice the .test postfix namespaces the event)
+     var entry_id = kdp.evaluate('{mediaProxy.entry.id}');
+     trackURL(entry_id);
+     kdp.kUnbind('.test');
+ });
+});
+
